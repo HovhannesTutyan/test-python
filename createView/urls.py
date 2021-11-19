d94309387940c0a9033a1b89ef8fb213e92f2e37
Admin <atutyanaz@gmail.com>
2021-11-19 10:37:35 +0400

    in urls extra_context added

--- createView/urls.py
+++ createView/urls.py
@@ -5,6 +5,6 @@ from student import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/', views.StudentCreateView.as_view(), name='stucreate'),
-    path('thanks/', views.ThanksTemplateView.as_view(), name='thankyou'),
+    path('thanks/', views.ThanksTemplateView.as_view(extra_context={'course':'Python'}), name='thankyou'),
     path('studetail/<int:pk>', views.StudentDetailView.as_view(), name='detail'),
 ]
