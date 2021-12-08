2fd1f5b230fe5b4141a226143a9aa9f331b62b85
Admin <atutyanaz@gmail.com>
2021-12-08 10:35:36 +0400

    "Assets page tests added"

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.driver.get('https://www.franckmuller.com/')
+        self.driver.get('https://www.cincopa.com/')
         time.sleep(3)
         self.driver.maximize_window()
         time.sleep(2)
     def home_page_title(self):
         get_title = self.driver.title
         print(get_title)
-        assert get_title == "Franck Muller Official Website - Haute Horlogerie Watches"
+        assert get_title == "Cincopa: Video Hosting, Photo Galleries, HTML Slideshows and More"
